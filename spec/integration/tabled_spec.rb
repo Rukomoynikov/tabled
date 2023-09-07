4a9c91c2cd934607666dbc61061ce2e32590dfdf
Maksim Rukomoynikov <max@apply4.com>
2023-09-07 21:14:13 +0100

    Refactor tests into cleaner structure

--- spec/integration/tabled_spec.rb
+++ spec/integration/tabled_spec.rb
@@ -4,7 +4,7 @@ require 'tabled'
 
 describe Tabled do
   context 'without frame' do
-    it 'prints table to console' do
+    it 'prints framed table to console when framed option is passed' do
       message = Factories::ProcessedData.with_default_row_separator.join("\n")
 
       expect do
@@ -12,38 +12,12 @@ describe Tabled do
       end.to output(message).to_stdout
     end
 
-    it 'prints table to console' do
+    it 'prints framed table to console without option' do
       message = Factories::ProcessedData.framed_default_row_separator.join("\n")
 
       expect do
         described_class.new(Factories::IncomingData.raw).print_to_console
       end.to output(message).to_stdout
-    end
-  end
-end
-
-describe 'Running tabled from console to print CSV file', type: :aruba do
-  context 'when called without file argument' do
-    specify do
-      expect do
-        system('./bin/tabled print')
-      end.to output(/File must be provided/).to_stdout_from_any_process
-    end
-  end
-
-  context 'when file doesn\'t exist' do
-    specify do
-      expect do
-        system('./bin/tabled print file.csv')
-      end.to output(/File doesn't exist/).to_stdout_from_any_process
-    end
-  end
-
-  context 'when file exist' do
-    specify do
-      expect do
-        system('./bin/tabled print spec/fixtures/starwars.csv')
-      end.to output(Factories::ProcessedData::Framed.star_wars_list).to_stdout_from_any_process
     end
   end
 end
--- spec/unit/tabled_spec.rb
+++ spec/unit/tabled_spec.rb
@@ -3,62 +3,52 @@
 require 'tabled'
 
 describe Tabled do
-  context 'without frame' do
+  describe 'columns width' do
     it 'returns correct columns width' do
-      expect(described_class.new(Factories::IncomingData.raw, framed: false).columns_width).to eq([13, 4, 46])
+      expect(described_class.new(Factories::IncomingData.raw).columns_width).to eq([13, 4, 46])
+    end
+
+    it 'can use terminal colors and it doesn\'t break table' do
+      tabled_instance = described_class.new(Factories::IncomingData.raw_with_rainbow,
+                                            framed: false)
+
+      expect(tabled_instance.columns_width).to eq([13, 4, 23])
     end
   end
 
-  it 'returns correct columns width' do
-    expect(described_class.new(Factories::IncomingData.raw).columns_width).to eq([13, 4, 46])
-  end
+  describe 'list of string in content' do
+    it 'returns list of strings accordingly to given data' do
+      expect(described_class.new(Factories::IncomingData.raw).content).to eq(Factories::ProcessedData.framed_default_row_separator)
+    end
 
-  it 'returns list of strings accordingly to given data' do
-    expect(
-      described_class.new(Factories::IncomingData.raw).content
-    ).to eq(
-      Factories::ProcessedData.framed_default_row_separator
-    )
-  end
+    it 'returns list of strings accordingly to given data when framed is false' do
+      expect(described_class.new(Factories::IncomingData.raw,
+                                 framed: false).content).to eq(Factories::ProcessedData.with_default_row_separator)
+    end
 
-  it 'returns list of strings accordingly to given data' do
-    tabled_with_titles = described_class.new(
-      Factories::IncomingData.raw,
-      titles: ['Cop name', 'Amount of warnings', 'Over the limit']
-    )
+    it 'returns list of strings accordingly to given data + titles' do
+      tabled_with_titles = described_class.new(
+        Factories::IncomingData.raw,
+        titles: ['Cop name', 'Amount of warnings', 'Over the limit']
+      )
 
-    expect(
-      tabled_with_titles.content
-    ).to match_array(
-      Factories::ProcessedData.with_titles
-    )
-  end
+      expect(
+        tabled_with_titles.content
+      ).to match_array(Factories::ProcessedData.with_titles)
+    end
 
-  it 'returns list of strings accordingly to given data' do
-    expect(
-      described_class.new(Factories::IncomingData.raw, row_separator: nil).content
-    ).to eq(
-      Factories::ProcessedData.framed_without_row_separator
-    )
-  end
+    it 'returns list of strings accordingly to given data when row separator is nil' do
+      expect(
+        described_class.new(Factories::IncomingData.raw, row_separator: nil).content
+      ).to eq(Factories::ProcessedData.framed_without_row_separator)
+    end
 
-  it 'can use Rainbow and doesn\'t break table' do
-    tabled_instance = described_class.new(Factories::IncomingData.raw_with_rainbow,
-                                          framed: false)
-
-    expect(tabled_instance.columns_width).to eq([13, 4, 23])
-  end
-
-  it 'returns list of strings accordingly to given data' do
-    expect(described_class.new(Factories::IncomingData.raw,
-                               framed: false).content).to eq(Factories::ProcessedData.with_default_row_separator)
-  end
-
-  it 'returns list of strings accordingly to given data' do
-    expect(
-      described_class.new(Factories::IncomingData.raw, row_separator: nil, framed: false).content
-    ).to eq(
-      Factories::ProcessedData.without_row_separator
-    )
+    it 'returns list of strings accordingly to given data when row separator is nil and framed is nil' do
+      expect(
+        described_class.new(Factories::IncomingData.raw, row_separator: nil, framed: false).content
+      ).to eq(
+        Factories::ProcessedData.without_row_separator
+      )
+    end
   end
 end
